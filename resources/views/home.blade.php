d01189e68b3836feccd8bb3b65e08f0352b6cf3a
Ezarsatria12 <Ezar3004>
2021-09-30 19:55:09 +0700

    3rd commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/homecustom.css"> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
-    
+    @include('partials.nav');
+    <div class="content">
+        <div class="container d-flex justify-content-xl-between">
+            <div class="col-sm-6 ">
+                <h1>Lorem ipsum dolor sit amet</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut feugiat bibendum dignissim mi. Quis convallis aliquet faucibus ornare euismod purus, consectetur amet elementum.</p>
+            </div>
+            <div class="col-sm-6">
+                <img src="img/intro.png" alt="">
+            </div>
+
+        </div>
+        <div class="box">
+
+        </div>
+        <div class="container">
+            <div class="col-sm-12">
+                <h3>Baru ditambahkan</h3>
+            </div>
+
+            <div class="col-sm-12">
+                <h3>Lorem ipsum dolor sit amet</h3>
+            </div>
+        </div>
+        <div class="container">
+            <div class="col-sm-12">
+                <h1>Lorem ipsum dolor sit amet</h1>
+            </div>
+
+        </div>
+    </div>
+    @include('partials.bottom');
 </body>
 </html>
---
+++ resources/views/partials/nav.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-sm">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <h3>Logo</h3>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/shop">Shop</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Help</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Blog</a>
+      </li>
+    </ul>
+  </div>
+</nav>
